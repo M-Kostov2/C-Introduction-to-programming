fe335b1be73c19a5800177a7d5a5f6d219d5598a
M-Kostov2 <135524907+M-Kostov2@users.noreply.github.com>
2024-11-11 22:39:09 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿namespace homework_2___social_platform_likes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name ="";
+            int count = 0;
+            string[] names = new string[2];
+            
+            
+            while (true)
+            {
+                Console.WriteLine("Enter name: ");
+                name = Console.ReadLine();
+
+                if (name =="")
+                {                 
+                    continue;                                  
+                }
+                if(count < 2 )
+                {
+                    names[count] = name;
+                    count++;
+                    if (count == 1)
+                    {
+                        Console.WriteLine($"{names[0]} liked your post");
+                        continue;
+                    }
+                    if (count == 2)
+                    {
+                        Console.WriteLine($"{names[0]} and {names[1]} liked your post");
+                        continue;
+                    }
+
+                }
+               
+                Console.WriteLine($"{names[0]}, {names[1]} and {count-1} liked your post");
+                count++;
+
+
+            }
+        }
+    }
+}
