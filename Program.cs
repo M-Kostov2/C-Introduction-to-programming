dacb5356788a1cef8873c035edd492bb32a2e52e
M-Kostov2 <135524907+M-Kostov2@users.noreply.github.com>
2024-11-11 22:41:33 +0200

    Homework 1 ex-2

--- Program.cs
+++ Program.cs
@@ -1,99 +1,45 @@
-﻿namespace homework_10._11._2024
+﻿namespace homework_2___social_platform_likes
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int h = int.Parse(Console.ReadLine());
-            string side = Console.ReadLine();
-
-
-            if (side == "left")
+            string name ="";
+            int count = 0;
+            string[] names = new string[2];
+            
+            
+            while (true)
             {
-               //declaring variables
-                string blanks = "";
-                string ast = "";
-                //writing first *
-                Console.Write($"{ast}{blanks}*");
-                Console.WriteLine();
+                Console.WriteLine("Enter name: ");
+                name = Console.ReadLine();
 
-                //writing all others
-                for (int i = 1; i < h - 1; i++)
-                {
-                    ast = "*";
-                    Console.Write($"{ast}{blanks}*");
-                    Console.WriteLine();
-                    
-                    blanks += " ";
-
+                if (name =="")
+                {                 
+                    continue;                                  
                 }
-
-                //writing last line
-                ast = "";
-                for (int i = 0; i < h; i++)
+                if(count < 2 )
                 {
-                    ast += "*";
-                }
-                Console.WriteLine(ast);
-            }
-            else if(side=="right")
-            {   
-                //declaring variabels
-                string blanks = "";
-                string ast = ""; 
-                string blanks2 ="";
+                    names[count] = name;
+                    count++;
+                    if (count == 1)
+                    {
+                        Console.WriteLine($"{names[0]} liked your post");
+                        continue;
+                    }
+                    if (count == 2)
+                    {
+                        Console.WriteLine($"{names[0]} and {names[1]} liked your post");
+                        continue;
+                    }
 
-                //making blanks(blank space less with 1)
-                for (int i = 0; i < h-1; i++)
-                {
-                    blanks +=" ";
                 }
                
-
-                int temp = h;
-                
-               //printing first *
-                Console.Write($"{blanks}{ast}*");
-                Console.WriteLine();
-
-                //printing all others
-                for (int i = 1; i < h-1; i++)
-                {   ast = "*";
-                     blanks = "";
-                    for (int k = temp-1; k > 1; k--)
-                    {
-                        
-                        blanks +=" ";
-                        
-                    }
-                     temp--;  
-                    //these 2 are just for debuging for seeing values in real time
-                    string nn = $"{blanks}{ast}{blanks2}*";
-                    int len = blanks.Length;
-                    //---------------------
-
-                    //printing next line----
-                    Console.Write($"{blanks}{ast}{blanks2}*");
-                    Console.WriteLine();
-                    
-                                    
-                    //growing the space between *       
-                    blanks2 +=" ";
-                
-                }
-                ast = "";
-                for (int i = 0; i < h; i++)
-                {
-                    ast += "*";
-                }
-                Console.WriteLine(ast);
-                
+                Console.WriteLine($"{names[0]}, {names[1]} and {count-1} liked your post");
+                count++;
 
 
-                
             }
-          
         }
-        //
     }
 }
