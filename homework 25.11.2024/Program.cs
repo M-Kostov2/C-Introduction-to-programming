520d6cde8993f33038df4631c5d141f68fa5e1e7
M-Kostov2 <135524907+M-Kostov2@users.noreply.github.com>
2024-11-25 22:13:05 +0200

    Add files via upload

---
+++ homework 25.11.2024/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace homework_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> names = new Queue<string> ();
+            Console.WriteLine("Write number of people");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Write {n} names");
+            for (int i = 0; i < n; i++)
+            {
+                names.Enqueue(Console.ReadLine());
+            }
+            while (names.Count != 0)
+            {    Console.WriteLine($"Serving: {names.Dequeue()}");
+                if (names.Count != 0)
+                {                    
+                    Console.WriteLine($"Next in Line: {names.Peek()}");
+                    continue;
+                }
+               
+            }
+        }
+    }
+}
