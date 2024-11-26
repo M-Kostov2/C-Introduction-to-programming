ce82f8b4c340e37f931baf91c5ef40cf99b36d2e
M-Kostov2 <135524907+M-Kostov2@users.noreply.github.com>
2024-11-26 23:49:46 +0200

    Add files via upload

---
+++ homework 26.11.2024/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using System.Threading.Tasks.Dataflow;
+
+namespace reading_and_writing_in_file
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Enter path to file.Like this (..\..\..\..\name of the file)");
+
+            //string path = Console.ReadLine();
+            string path = @"..\..\..\..\test.txt";
+            
+
+             StreamReader sReader = new StreamReader(path);
+
+             
+
+             string text = sReader.ReadToEnd();       
+             string[] senteces = text.Split(new char[3] {'.', '!', '?' });
+             string[] words = text.Split(new char[4] {' ','.','!','?'});
+
+             sReader = new StreamReader(path);
+             string line1 = sReader.ReadLine();
+             string line2 = sReader.ReadLine();
+             string line3 = sReader.ReadLine();
+
+            
+            FileStream file = File.Create(@"..\..\..\..\test2.txt");
+            file.Close();
+         
+            File.AppendAllText(@"..\..\..\..\test2.txt",$"Sentences count{senteces.Length - 1}");            
+            file.Close();
+        
+            File.AppendAllText(@"..\..\..\..\test2.txt",$"Words count{words.Length - 1}");            
+            file.Close();
+            
+            File.AppendAllText(@"..\..\..\..\test2.txt",$"{String.Join(".",line1,line2,line3)}");
+            file.Close();
+            
+            
+           
+           
+        }
+    }
+}
