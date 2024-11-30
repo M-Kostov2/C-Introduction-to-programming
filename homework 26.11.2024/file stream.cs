0ea5b5158a05561189383ef92e3961d03b7656ba
M-Kostov2 <135524907+M-Kostov2@users.noreply.github.com>
2024-11-30 11:42:09 +0200

    Add files via upload

---
+++ homework 26.11.2024/file stream.cs
@@ -0,0 +1,15 @@
+﻿namespace exersice_1_file_stream
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   //1exersice
+
+            string path = "C://UnivercityC#//..//textdocument11.txt";
+            bool file =  File.Exists(path);
+            Console.WriteLine(file);
+
+
+        }
+    }
+}
