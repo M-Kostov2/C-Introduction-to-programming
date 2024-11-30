84031dbf166463b4ab8c96edfcd1dc8877c42ba0
M-Kostov2 <135524907+M-Kostov2@users.noreply.github.com>
2024-11-30 11:40:53 +0200

    Add files via upload

---
+++ homework 26.11.2024/Writing in csv file.cs
@@ -0,0 +1,32 @@
+﻿namespace writing_in_csv_file
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //FileStream file = File.Create(@"file.csv");
+
+            StreamReader sr = new StreamReader(@"file.csv");
+            List<int> MaxPrice = new List<int>();
+            double quantity = 0;
+            string mostSold = "";
+            while (sr.EndOfStream == false)
+            {
+                string[] line = sr.ReadLine().Split("/");
+                Console.WriteLine($"Product {line[0]}");
+                double price = (double.Parse(line[1]) * double.Parse(line[2]));
+                Console.WriteLine($"Quantity * Price = {price:f2}");
+                MaxPrice.Add(int.Parse(line[2]));
+                
+                if (double.Parse(line[2]) > quantity)
+                {
+                    quantity = double.Parse(line[2]);
+                    mostSold = line[0];
+                }
+                
+            }
+            sr.Close();
+            Console.WriteLine($"Most sold product: {mostSold} quantity-{quantity}");
+        }
+    }
+}
