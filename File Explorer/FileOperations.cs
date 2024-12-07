645c04e72a31e60fe59976778dd400bee89315ee
M-Kostov2 <135524907+M-Kostov2@users.noreply.github.com>
2024-12-07 10:11:35 +0200

    Add files via upload

---
+++ File Explorer/FileOperations.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace File_Explorer
+{
+    public class FileOperations
+    {
+        //This class gives file operation methods
+
+
+        //Check if file exist
+        public bool FileExist(string DirectoryPath,string Name)
+        {
+            bool exist = Directory.Exists(Path.Combine(DirectoryPath,Name));
+
+            return exist;
+        }
+
+        //Creates new directory(folder) 
+        public void CreateNewDirectory(string DirectoryPath, string Name)//c:/files
+        {
+            Directory.CreateDirectory(Path.Combine(DirectoryPath, Name));
+            Console.WriteLine("Directory Created!");
+        }
+
+        //Creates file
+        public void CreateNewFile(string DirectoryPath, string Name)
+        { 
+          File.Create(Path.Combine(DirectoryPath, Name));
+          Console.WriteLine("File Created!");        
+        }
+
+        public void ExplorerOptions()
+        {
+            Console.WriteLine("1.Show Content");
+            Console.WriteLine("2.Create Directory");
+            Console.WriteLine("3.Create File");
+            Console.WriteLine("4.Delete Directory/File");
+            Console.WriteLine("5.Set Directory");
+            Console.WriteLine("6.Directory/File Exist");
+            Console.WriteLine("7.Exit");
+        }
+
+    }
+}
---
+++ File Explorer/Program.cs
@@ -0,0 +1,67 @@
+﻿using static File_Explorer.FileOperations;
+using System.IO;
+
+namespace File_Explorer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //get info 
+            //проверка на дир дали съществува ако не нова папка
+            //добавяме нови папки с числата от месеците
+            //първата папка да има папка -01 (10файла) не повече ако не нова папка -02
+            //взависимост от месеца
+
+
+
+            // changing directory based on input then importing methods to work with the program 
+            //implementing while cycle (end when you choose exit till then program circles in if else stastments)
+            FileOperations directory = new FileOperations();
+            Stack<string> Paths = new Stack<string>();
+            string OriginalDirectory = "C:/";
+
+            string command = "";
+
+
+
+            Console.WriteLine("File Explorer");
+            directory.ExplorerOptions();
+            command = Console.ReadLine();
+            Environment.CurrentDirectory = (OriginalDirectory);
+
+            while (command != "Exit")
+            {
+
+
+
+
+
+                if (check)
+                {
+                    //newdir
+                    //newDir//01//1-01
+
+
+                }
+                else if (message == "Folder doesn't exist")
+                {
+                    directory.NewDir();
+                }
+
+
+
+
+
+
+
+            }
+
+
+
+
+
+
+        }
+    }
+}
